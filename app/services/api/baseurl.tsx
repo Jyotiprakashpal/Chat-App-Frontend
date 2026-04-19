f209766666800e6bc8abeddeef156ef448c90183
Jyotiprakash Pal <jyotiprakash.pal@acxiomconsulting.com>
2026-04-19 16:47:51 +0530

    Use Online URL

--- app/services/api/baseurl.tsx
+++ app/services/api/baseurl.tsx
@@ -1,6 +1,6 @@
-// export const BASE_URL = 'http://192.168.76.248:5000/api';
-// export const BASE_URL = 'http://192.168.77.52:5000/api'; //online ip address of the backend server
 
-export const BASE_URL = 'http://localhost:5000/api';
+// export const BASE_URL = 'http://localhost:5000/api';//local ip address of the backend server
+
+export const BASE_URL = 'https://chat-app-backend-78lw.onrender.com/api'; //online ip address of the backend server
 
 
